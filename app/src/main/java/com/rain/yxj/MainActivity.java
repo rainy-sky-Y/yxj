a512c648a3205a121dc680977a145f470786affd
rain <2509671118@>
2023-05-31 16:01:17 +0800

    第二次提交

--- app/src/main/java/com/rain/yxj/MainActivity.java
+++ app/src/main/java/com/rain/yxj/MainActivity.java
@@ -1,15 +1,18 @@
 package com.rain.yxj;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.rain.yxj.net.CallBack;
 import com.rain.yxj.net.OkhttpUtils;
@@ -27,8 +30,13 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView ls;
     private Button search;
+    private View bottom;
+    private TextView tv;
+    private ProgressBar pb;
     List<Product> products = new ArrayList<>();
     ProductAdapter pa;
+
+    int pageIndex = 1, pageNum = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         initProduct();
 
         pa = new ProductAdapter(MainActivity.this,R.layout.product_item,products);
+        ls.addFooterView(bottom);
         ls.setAdapter(pa);
     }
 
@@ -48,6 +57,9 @@ public class MainActivity extends AppCompatActivity {
 
         ls = findViewById(R.id.ls);
         search = findViewById(R.id.search);
+        bottom = getLayoutInflater().inflate(R.layout.bottom, null);
+        pb = (ProgressBar) bottom.findViewById(R.id.progressBar1);
+        tv = (TextView) bottom.findViewById(R.id.textView);
 
     }
     //初始化事件
@@ -74,16 +86,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //为listview设置滑动监听
+        ls.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                // TODO Auto-generated method stub
+                // 判断滚动是否停止
+                if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
+                    //判断界面上显示的最后一项item的position，是否等于item的总个数减1（item的position从0开始），如果是，说明滑动到了底部。
+                    if (view.getLastVisiblePosition() == (view.getCount() - 1)) {
+                        /*
+                         * 如果程序运行到这里，说明滑动到了底部，下面显示加载效果，通过线程加载数据
+                         */
+                        pageIndex++;
+                        initProduct();
+                    }
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView absListView, int i, int i1, int i2) {
+
+            }
+        });
     }
     //初始化数据
     private void initProduct(){
-        int pageIndex = 1, pageNum = 20;
+
         String url = "http://goods.yuanxiaojiang.com/api/product/getproductList?pageIndex="+pageIndex+"&pageNum="+pageNum;
         OkhttpUtils.getInstance().doGet(url, new CallBack() {
             @Override
             public void onSuccess(String result) {
                 try {
                     JSONArray pro_list = new JSONObject(result).getJSONArray("Data");
+                    if (pro_list.length()==0){
+                        tv.setVisibility(View.VISIBLE);
+                        pb.setVisibility(View.GONE);
+                    }
                     Product product;
                     for (int i = 0; i < pro_list.length(); i++) {
                         JSONObject object = (JSONObject) pro_list.opt(i);
